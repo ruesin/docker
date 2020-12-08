c568bc7db26d13d15a8cfabbbb05a9aa042c4166
ruesin <ruesin@gmail.com>
2020-12-08 16:52:54 +0800

    update ymal

--- develop/docsin.sh
+++ develop/docsin.sh
@@ -1,52 +1,62 @@
 #!/bin/sh
 
-DOCSIN_WORK_DIR=$(cd $(dirname $0); pwd)
+# 工作目录
+export DOCSIN_WORKER_DIR=$(cd $(dirname $0); pwd)
 
-if [ -f "${DOCSIN_WORK_DIR}/config" ];then
-    source ${DOCSIN_WORK_DIR}/config
+if [ -f "${DOCSIN_WORKER_DIR}/config" ];then
+    source ${DOCSIN_WORKER_DIR}/config
 fi
 
+# 应用名
 DOCSIN_APP_NAME=$1
 
+# 操作
 DOCSIN_OPERATION=$2
 
-# Create and start containers
+# 启动应用
 docSinStart() {
     # 项目软连接
     if [ ! -n "$DOCSIN_YAML_FILE" ] ;then
         DOCSIN_PROJECT_PATH="${HOME}/projects/"
     fi
-    if [ ! -d "${DOCSIN_WORK_DIR}/projects/" ];then
+    if [ ! -d "${DOCSIN_WORKER_DIR}/projects/" ];then
         if [ -d "$DOCSIN_PROJECT_PATH" ]; then
-            ln -s ${DOCSIN_PROJECT_PATH} ${DOCSIN_WORK_DIR}/projects
+            ln -s ${DOCSIN_PROJECT_PATH} ${DOCSIN_WORKER_DIR}/projects
         else 
-            mkdir ${DOCSIN_WORK_DIR}/projects
+            mkdir ${DOCSIN_WORKER_DIR}/projects
         fi
     fi
+
+    # 启动命令
     docker-compose -f "${DOCSIN_YAML_FILE}" -p ${DOCSIN_APP_NAME} up -d
 }
 
-# Stop services
+# 停止应用
 docSinStop() {
     docker-compose -f "${DOCSIN_YAML_FILE}" -p ${DOCSIN_APP_NAME} stop
 }
 
-# Stop and remove containers, networks, images, and volumes
+# 停止并清理容器
 docSinClear() {
     docker-compose -f "${DOCSIN_YAML_FILE}"  -p ${DOCSIN_APP_NAME} down
 }
 
-# get yaml file
+# 获取yaml文件
 docSinYaml() {
     if [ ! -n "$DOCSIN_YAML_FILE" ] ;then
-        if [ ! -f "${DOCSIN_WORK_DIR}/yaml/${DOCSIN_APP_NAME}.yml" ];then
-            if [ ! -f "${DOCSIN_WORK_DIR}/yaml/example/${DOCSIN_APP_NAME}.yml" ];then
-                echo "Has not [${DOCSIN_APP_NAME}.yml]!"
+        if [ ! -f "${DOCSIN_WORKER_DIR}/yaml/${DOCSIN_APP_NAME}/docker-compose.yml" ];then
+            if [ ! -f "${DOCSIN_WORKER_DIR}/yaml/example/${DOCSIN_APP_NAME}/docker-compose.yml" ];then
+                echo "Has not [${DOCSIN_APP_NAME}] yaml!"
                 exit 1
             fi
-            cp ${DOCSIN_WORK_DIR}/yaml/example/${DOCSIN_APP_NAME}.yml ${DOCSIN_WORK_DIR}/yaml/${DOCSIN_APP_NAME}.yml
+
+            if [ ! -d "${DOCSIN_WORKER_DIR}/yaml/${DOCSIN_APP_NAME}/" ] ;then
+                mkdir ${DOCSIN_WORKER_DIR}/yaml/${DOCSIN_APP_NAME}
+            fi
+
+            cp ${DOCSIN_WORKER_DIR}/yaml/example/${DOCSIN_APP_NAME}/docker-compose.yml ${DOCSIN_WORKER_DIR}/yaml/${DOCSIN_APP_NAME}/docker-compose.yml
         fi
-        DOCSIN_YAML_FILE="${DOCSIN_WORK_DIR}/yaml/${DOCSIN_APP_NAME}.yml"
+        DOCSIN_YAML_FILE="${DOCSIN_WORKER_DIR}/yaml/${DOCSIN_APP_NAME}/docker-compose.yml"
     fi
 }
 
@@ -55,6 +65,7 @@ if [ ! -n "$DOCSIN_APP_NAME" ] ;then
     exit 1
 fi
 
+# 获取yaml文件
 docSinYaml
 
 if [ "$DOCSIN_OPERATION" == "start" ]; then
